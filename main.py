bfaad4aaaefb9fde0975ac1ab11df24333117da8
Slayer <chistyakov.andrey.vladimirovic@gmail.com>
2026-06-22 21:04:45 +0500

    modified date in widget.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 card = str(input("Введите свой банковский аккаунт или номер карты: "))
+date_string = str(input("Введите дату:  "))
 
 if "Счет" in card:
     text_part, number_part = mask_account_card(card)
@@ -11,3 +12,5 @@ else:
     text_part, number_part = mask_account_card(card)
     masked_card_number = get_mask_card_number(number_part)
     print(text_part, masked_card_number)
+
+print(get_date(date_string))
