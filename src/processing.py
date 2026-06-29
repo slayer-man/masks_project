5ecb983923560cbd196fb1fdeb606fa45370d49e
Slayer <chistyakov.andrey.vladimirovic@gmail.com>
2026-06-29 22:03:43 +0700

    add README.md in homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from tests.tests_dict import my_list_dict
 
 
 def filter_by_state(my_list_dict: list, state: str = "EXECUTED") -> list[str]:
@@ -23,27 +24,14 @@ def sort_by_date(transactions: list, reverse: bool = True) -> list:
 
     return sorted_transactions
 
+    # Примеры использования и проверки функций
 
-# Примеры использования и проверки функций
 
 if __name__ == "__main__":
 
-# Тестовые данные
-
-    test_data = [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
-
     print("По умолчанию (state='EXECUTED'):")
 
-    result_executed = filter_by_state(test_data)
+    result_executed = filter_by_state(my_list_dict)
 
     for item in result_executed:
 
