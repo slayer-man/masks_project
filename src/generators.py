65f27a4a556446be3d5f63426529f06d76e65fbd
Slayer <chistyakov.andrey.vladimirovic@gmail.com>
2026-07-17 21:23:39 +0700

    modified generators.py, test_generators.py & README.md

--- src/generators.py
+++ src/generators.py
@@ -5,22 +5,31 @@ from data.data_dict_generators import transactions
 
 
 # Генератор банковских карт
-def card_number_generator(prefix: str, length: int = 16) -> Any:
+def card_number_generator(start_val: int, end_val: int, prefix: str, length: int = 16) -> Any:
     """Генератор номеров карт. prefix - первые цифры в виде строки length - общая длина номера карты"""
-    zeros_count = length - len(prefix)
-    # Задаем начальное значение последних цифр (.count())
-    for counter in itertools.count(1):
-        counter_str = f"{counter:0{zeros_count}d}"
-        card_number = "".join(prefix + counter_str)
-        card_num = " ".join([card_number[i : i + 4] for i in range(0, len(card_number), 4)])
-        yield card_num
+
+    # Перебор диапазона от начального до конечного значения
+    for current_num in range(start_val, end_val + 1):
+
+        # Вычисляем сколько случайных цифр нужно дописать, чтобы достичь длины карты
+        num_to_fill = length - len(prefix)
+
+        for counter in itertools.count(start_val):
+            counter_str = f"{counter:0{num_to_fill}d}"
+            card_number = "".join(prefix + counter_str)
+            card_num = " ".join([card_number[i : i + 4] for i in range(0, len(card_number), 4)])
+            yield card_num
 
 
-# Создаем генератор номеров, всего 16 цифр (prefix - задает начальные цифры(до 14 цифр))
-card = card_number_generator(prefix="", length=16)
-# Вывод n - номеров.
-for _ in range(5):
-    print(next(card))
+# --- Пример использования ---
+if __name__ == "__main__":
+    start = 1  # Начальное значение диапазона
+    end = 5  # Конечное значение диапазона
+
+    card = card_number_generator(start, end, prefix="0", length=16)
+
+    for _ in range(end + 1 - start):
+        print(next(card))
 
 
 # Функция фильтр
