e318b99df4e622f944cee8dbd7a01f17223b613d
Slayer <chistyakov.andrey.vladimirovic@gmail.com>
2026-07-18 23:09:39 +0700

    modified generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,24 +1,17 @@
-import itertools
-from typing import Any
+from typing import Any, Dict, Generator, Iterator, List
 
 from data.data_dict_generators import transactions
 
 
 # Генератор банковских карт
-def card_number_generator(start_val: int, end_val: int, prefix: str, length: int = 16) -> Any:
+def card_number_generator(start_val: int, end_val: int) -> Generator[str, None, None]:
     """Генератор номеров карт. prefix - первые цифры в виде строки length - общая длина номера карты"""
 
     # Перебор диапазона от начального до конечного значения
     for current_num in range(start_val, end_val + 1):
-
-        # Вычисляем сколько случайных цифр нужно дописать, чтобы достичь длины карты
-        num_to_fill = length - len(prefix)
-
-        for counter in itertools.count(start_val):
-            counter_str = f"{counter:0{num_to_fill}d}"
-            card_number = "".join(prefix + counter_str)
-            card_num = " ".join([card_number[i : i + 4] for i in range(0, len(card_number), 4)])
-            yield card_num
+        card_number = f"{current_num:016d}"
+        card_num = " ".join([card_number[i:i + 4] for i in range(0, len(card_number), 4)])
+        yield card_num
 
 
 # --- Пример использования ---
@@ -26,14 +19,14 @@ if __name__ == "__main__":
     start = 1  # Начальное значение диапазона
     end = 5  # Конечное значение диапазона
 
-    card = card_number_generator(start, end, prefix="0", length=16)
+    card = card_number_generator(start, end)
 
     for _ in range(end + 1 - start):
         print(next(card))
 
 
 # Функция фильтр
-def filter_by_currency(transactions: dict, currency: str) -> Any:
+def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> Iterator[Dict[str, Any]]:
     """Функция фильтрующяя по currency"""
 
     for x in transactions:
@@ -48,7 +41,7 @@ for _ in range(3):
 
 
 # Генератор транзакций
-def transaction_descriptions(transactions: list[dict[str, object]]) -> Any:
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Iterator[str]:
     """Генератор выводящий данные по 'description'."""
     for i, t in enumerate(transactions):
         transactions_name = t["description"]
